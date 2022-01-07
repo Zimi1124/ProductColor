3da6c7ca8a1b113e37501abe7e531e9b0c903cf4
Zimi1124 <Zimi1124@gmail.com>
2022-01-07 02:07:46 +0100

    Test Behat

---
+++ tests/Behat/Pages/CreateProductPage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Behat\Pages;
+
+use FriendsOfBehat\PageObjectExtension\Page\SymfonyPage;
+
+class CreateProductPage extends SymfonyPage implements CreateProductPageInterface
+{
+    public function getRouteName(): string
+    {
+        return 'sylius_admin_product_create_simple';
+    }
+
+    public function setColor(string $color):void
+    {
+        $this->getDocument()->selectFieldOption("sylius_product_color", $color);
+    }
+}
